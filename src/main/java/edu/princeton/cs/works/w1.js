44390badb9d44246e8b6fbb480811fc4fffb85b2
nivendha <knivendha@gmail.com>
2017-03-09 22:56:35 +0530

    dynamic programing

--- src/main/java/edu/princeton/cs/works/w1.js
+++ src/main/java/edu/princeton/cs/works/w1.js
@@ -22,7 +22,7 @@ var qu={}
 qu.t = [0,1,2,3,4,5,6,7,8,9];
 
 qu.getRoot = function(i){
-	while(i!=qu.t[i]){
+	while(qu.t[i]!=i){
 		i = qu.t[i];
 	}
 	return i;
@@ -39,7 +39,7 @@ qu.union = function(p,q){
 var qu={}
 
 qu.t = [0,1,2,3,4,5,6,7,8,9];
-
+qu.sz = [1,1,1,1,1,1,1,1,1];
 qu.getRoot = function(i){
 	while(i!=qu.t[i]){
 		i = qu.t[i];
@@ -50,6 +50,14 @@ qu.getRoot = function(i){
 qu.union = function(p,q){
 	var i = qu.getRoot(p);
 	var j = qu.getRoot(q);
-	qu.t[i] = j;
-	return qu.t;
+	if(i==j){
+		return	qu.t;
+	}
+	if(qu.sz[j]>qu.s[i]){
+		qu.t[i] = j;
+		qu.sz[j]=qu.sz[j]+qu.sz[i];
+	}else{
+		qu.t[j] = i;
+		qu.sz[i]=qu.sz[i]+qu.sz[j];
+	}
 }
