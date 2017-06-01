848eb557920ed6e7ad8bc07f2faf574eb1a4b01b
Nivendha Kannan <nivendha.kannan@imaginea.com>
2017-06-01 17:31:31 +0530

    dp

--- src/main/java/edu/princeton/cs/g4g/practice/rod-cutting.js
+++ src/main/java/edu/princeton/cs/g4g/practice/rod-cutting.js
@@ -6,9 +6,42 @@ var cost =[3,5,2,12,2];
 /*
 logic
 
-1) Get all the combinations of the length => whoes sum mathes to the required 
+1) Get all the combination of the length => whoes sum mathes to the required 
 2) for each combination =>product the length with cost and sum them 
-3) get the min val sum of all the combinations
+3) get the min sum of all the combination
 
+
+
+1)get length combination
+
+ip=>'[a,b,c]'
+
+a ----> aa -->aa.a	aa.b	aa.c
+		
+		ab -->ab.a	ab.b	ab.c
+		
+		ac -->ac.a 	ac.b	ac.c
+
+b ----> ba -->ba.a	ba.b	ba.c
+		
+		bb -->bb.a	bb.b	bb.c
+		
+		bc -->bc.a 	bc.b	bc.c
+
+a ----> ca -->ca.a	ca.b	ca.c
+		
+		cb -->cb.a	cb.b	cb.c
+		
+		cc -->cc.a 	cc.b	cc.c
 */
+
+function combination(str,ip,n){
+
+}
+
+
+
+
+
+
 
