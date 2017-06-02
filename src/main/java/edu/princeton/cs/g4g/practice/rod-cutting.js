903e7d9af3de5d896e2d3d046da7a98ac95d97e2
Nivendha Kannan <nivendha.kannan@imaginea.com>
2017-06-02 15:18:24 +0530

    dp

--- src/main/java/edu/princeton/cs/g4g/practice/rod-cutting.js
+++ src/main/java/edu/princeton/cs/g4g/practice/rod-cutting.js
@@ -1,7 +1,5 @@
 /*rod cuting problem*/
 
-var length =[1,2,5,7,10];
-var cost =[3,5,2,12,2];
 
 /*
 logic
@@ -33,15 +31,117 @@ a ----> ca -->ca.a	ca.b	ca.c
 		cb -->cb.a	cb.b	cb.c
 		
 		cc -->cc.a 	cc.b	cc.c
+
+//need to find a recursive function , which when called with 3 arguments --> gives 9 calls stact --> which gives 27 calls
+
+1) Depth = 3;newlist =[a,b,c];list=['a','b','c'];
+	op-> ['aa','ab','ac','ba','bb','bc','ca','cb','cc']
+
+2) depth =2 ;newlis = op ; list=['a','b','c'];
+	op-->['aaa','aab','aac',.................'ccb','ccc']
+
+3) set at depth =1 -> val = newlist and return
 */
 
-function combination(str,ip,n){
+function combination(list,newList,depth,val){
+	var op =[];
+	debugger;
+	while(depth>1){
 
+		for(var i=0;i<newList.length;i++){
+			
+			var str = newList[i];
+			for(var j=0;j<list.length;j++){
+				var newStr = str + list[j];
+				op.push(newStr);
+			}
+
+		}
+		depth--;
+		val = combination(list,op,depth,val);
+		return val;
+	}
+	if(depth == 1){
+		console.log(newList);
+		val = newList;
+	}
+	return val;	
+}
+/*var ip = 'abc';
+var value = combination(ip.split(''),ip.split(''),ip.length);*/
+
+function combinationModified(list,newList,depth,required,val){
+	var op =[];
+	debugger;
+	while(depth>1){
+
+		for(var i=0;i<newList.length;i++){
+			
+			var str = newList[i];
+			for(var j=0;j<list.length;j++){
+				var newStr = str +','+ list[j];
+				op.push(newStr);
+			}
+
+		}
+		depth--;
+		val = combinationModified(list,op,depth,required,val);
+		return val;
+	}
+	if(depth == 1){
+		debugger;
+		for(var m=0;m<newList.length;m++){
+			var item = newList[m].split(',');
+			var sum = 0;
+			for(var n=0;n<item.length;n++){
+				sum+=parseInt(item[n]);
+			}
+			if(sum===required){
+					val.push(item);
+			}	
+		}
+	}
+	return val;	
 }
 
+function rodCutting(l,cost,required){
+	debugger;
+	//create a cost length map for easy use
+	var map = {}
+	for(var i=0;i<l.length;i++){
+		map[l[i]] = cost[i];
+	}
 
+	var lengthCombinations;
+	lengthCombinations = combinationModified(l,l,l.length,required,[]);
+	debugger;
 
+	var productArrayMap =[];
+	for(var i=0;i<lengthCombinations.length;i++){
+		var set = lengthCombinations[i];
+		var costSum =0;
+		for(var j=0;j<set.length;j++){
+			costSum += parseInt(map[set[j]]);
+		}
+		productArrayMap[i] = {
+			'cost' : costSum,
+			'combination' : set
+		}
+	}
 
+	//set some intialmincost
+	var minCostObj = productArrayMap[0];
 
+	for(var i=1;i<productArrayMap.length;i++){
+		if(productArrayMap[i].cost<minCostObj.cost){
+			 minCostObj = productArrayMap[i];
+		}
+	}
 
+	return minCostObj;
+}
 
+var length =[1,2,5,7,10];
+var cost =[3,5,2,12,2];
+var required = 15;
+rodCutting(length,cost,required);
